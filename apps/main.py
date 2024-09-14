87c7bd4599ce7457cf2830ec7fff02e643236589
BangAjus <gunggus2367@gmail.com>
2024-09-14 23:17:33 +0800

    second commit

--- apps/main.py
+++ apps/main.py
@@ -7,36 +7,51 @@ app = Flask('ML_API')
 
 @app.route("/", methods=["GET"])
 def index():
+    if request.method != "GET":
 
-    if not request:
-        return jsonify({"status":
-                        { "code": 400,
-                          "message":"Couldn't fetch the API"
-                               }
-                    },
-                    400)
-    data = request.json
-    features = process_data_after_json(data)
-    features_scaled = min_max_scaling(features)
-
-    labels = model.clustering(features_scaled,
-                              int(data['days']))
-    labels = labels.reshape(-1, 1)
+        return jsonify({
+            "status":
+            {
+                "code": 405,
+                "message": "Method not allowed",
+            },
+            "data": None,
+        }), 405
     
-    features = inverse_scaling(features)
-    features = concatenate((features, labels),
-                           axis=1)
+    else:
 
-    return jsonify({"status":{
-                        "code":200,
-                        "message":"Success fetching the API"
-                        },
+        if not request:
+            return jsonify({
+                "status":
+                { 
+                    "code": 400,
+                    "message":"Couldn't fetch the API"
                         
-                    "data":[{'latitude':i,
-                             'longitude':j,
-                             'label':k} \
-                             for i, j, k in \
-                                 zip(features[:, 0],
-                                     features[:, 1],
-                                     features[:, 2])]
-                   })
+                }
+            }), 400
+        
+        data = request.json
+        features = process_data_after_json(data)
+        features_scaled = min_max_scaling(features)
+
+        labels = model.clustering(features_scaled,
+                                int(data['days']))
+        labels = labels.reshape(-1, 1)
+        
+        features = inverse_scaling(features)
+        features = concatenate((features, labels),
+                            axis=1)
+
+        return jsonify({"status":{
+                            "code":200,
+                            "message":"Success fetching the API"
+                            },
+                            
+                        "data":[{'latitude':i,
+                                'longitude':j,
+                                'label':k} \
+                                for i, j, k in \
+                                    zip(features[:, 0],
+                                        features[:, 1],
+                                        features[:, 2])]
+                    })
