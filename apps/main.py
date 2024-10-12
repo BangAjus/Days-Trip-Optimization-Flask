36dd8fae9f7ffb11a4c313321f6e2d1bc70e27eb
BangAjus <72916363+BangAjus@users.noreply.github.com>
2024-10-12 14:39:57 +0800

    Adding coloring feature

--- apps/main.py
+++ apps/main.py
@@ -27,12 +27,15 @@ def index():
     features = process_data_after_json(data)
     features_scaled = min_max_scaling(features)
 
-    labels = model.clustering(features_scaled,
-                            int(data['days']))
+    labels, coloring = model.clustering(features_scaled,
+                                        int(data['days']))
     labels = labels.reshape(-1, 1)
+    coloring = coloring.reshape(-1, 1)
     
     features = inverse_scaling(features)
     features = concatenate((features, labels),
+                        axis=1)
+    features = concatenate((features, coloring),
                         axis=1)
 
     return jsonify({"status":{
